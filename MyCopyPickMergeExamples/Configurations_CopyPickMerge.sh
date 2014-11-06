e372138e66eb431cbb2b7bb4132bd0f5618a2c3b
Piet Verwilligen <piet@blacksad.(none)>
2014-11-06 18:32:35 +0100

    first commit

---
+++ MyCopyPickMergeExamples/Configurations_CopyPickMerge.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+
+# cmsRun copyPickMerge_cfg.py inputFiles=/store/data/Run2012A/SingleMu/AOD/22Jan2013-v1/20000/9ADB6751-9B6F-E211-BEE6-1CC1DE1CE026.root outputFile=Run190707_Lumi7_numEvent1000.root maxEvents=1000
+
+# cmsRun copyPickMerge_cfg.py inputFiles=/store/data/Run2011A/SingleMu/AOD/08Nov2011-v1/0000/00D922FB-CE0B-E111-A75D-00237DA1494E.root outputFile=00D922FB-CE0B-E111-A75D-00237DA1494E.root maxEvents=15000
+
+
+# DAS Query :: file run=216425 dataset=/Commissioning/Commissioning2013-PromptReco-v1/RECO
+# cmsRun copyPickMerge_cfg.py inputFiles=/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/D0D06494-C849-E311-B030-003048F23D74.root outputFile=GRIN_216425_D0D06494-C849-E311-B030-003048F23D74.root maxEvents=-1
+# cmsRun copyPickMerge_cfg.py inputFiles=/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/E84A85EB-C849-E311-8FCB-002481E0DC70.root outputFile=GRIN_216425_E84A85EB-C849-E311-8FCB-002481E0DC70.root maxEvents=-1
+
+# DAS Query :: file run=216424 dataset=/Commissioning/Commissioning2013-PromptReco-v1/RECO
+cmsRun copyPickMerge_cfg.py inputFiles=/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/1435374E-E449-E311-AD72-0025901E3D64.root outputFile=GRIN_216424_1435374E-E449-E311-AD72-0025901E3D64.root maxEvents=-1
+cmsRun copyPickMerge_cfg.py inputFiles=/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/620101F2-E349-E311-BB7B-003048F0E1CC.root outputFile=GRIN_216424_620101F2-E349-E311-BB7B-003048F0E1CC.root maxEvents=-1
+
+# DAS Query :: file run=216473 dataset=/Commissioning/Commissioning2013-PromptReco-v1/RECO
+
+cmsRun copyPickMerge_cfg.py inputFiles=/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/26E5E9B9-6D49-E311-BACE-0030486780E4.root outputFile=GRIN_216473_26E5E9B9-6D49-E311-BACE-0030486780E4.root maxEvents=-1
+cmsRun copyPickMerge_cfg.py inputFiles=/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/402B6169-6E49-E311-BAF1-003048D2BF26.root outputFile=GRIN_216473_402B6169-6E49-E311-BAF1-003048D2BF26.root maxEvents=-1
+cmsRun copyPickMerge_cfg.py inputFiles=/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/4CFC12AD-6E49-E311-ACB6-003048D2BEE6.root outputFile=GRIN_216473_4CFC12AD-6E49-E311-ACB6-003048D2BEE6.root maxEvents=-1
+cmsRun copyPickMerge_cfg.py inputFiles=/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/66E057B1-6D49-E311-9CFC-003048C90C08.root outputFile=GRIN_216473_66E057B1-6D49-E311-9CFC-003048C90C08.root maxEvents=-1
+cmsRun copyPickMerge_cfg.py inputFiles=/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/6C7EC9AE-6E49-E311-ADCF-003048D2C020.root outputFile=GRIN_216473_6C7EC9AE-6E49-E311-ADCF-003048D2C020.root maxEvents=-1
---
+++ MyCopyPickMergeExamples/Configurations_xrdcp.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+# DAS Query :: file run=216473 dataset=/Commissioning/Commissioning2013-PromptReco-v1/RECO
+# xrdcp root://eoscms//eos/cms/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/26E5E9B9-6D49-E311-BACE-0030486780E4.root .
+# xrdcp root://eoscms//eos/cms/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/402B6169-6E49-E311-BAF1-003048D2BF26.root .
+xrdcp root://eoscms//eos/cms/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/4CFC12AD-6E49-E311-ACB6-003048D2BEE6.root .
+xrdcp root://eoscms//eos/cms/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/66E057B1-6D49-E311-9CFC-003048C90C08.root .
+xrdcp root://eoscms//eos/cms/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/6C7EC9AE-6E49-E311-ADCF-003048D2C020.root .
+
+# DAS Query :: file run=216425 dataset=/Commissioning/Commissioning2013-PromptReco-v1/RECO
+# xrdcp root://eoscms//eos/cms/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/D0D06494-C849-E311-B030-003048F23D74.root .
+# xrdcp root://eoscms//eos/cms/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/E84A85EB-C849-E311-8FCB-002481E0DC70.root .
+
+# DAS Query :: file run=216424 dataset=/Commissioning/Commissioning2013-PromptReco-v1/RECO
+# xrdcp root://eoscms//eos/cms/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/1435374E-E449-E311-AD72-0025901E3D64.root .
+# xrdcp root://eoscms//eos/cms/store/data/Commissioning2013/Commissioning/RECO/PromptReco-v1/00000/620101F2-E349-E311-BB7B-003048F0E1CC.root .
+
